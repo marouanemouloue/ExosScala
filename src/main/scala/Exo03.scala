c5e2f9a5754db07292421d69b04c02acd6501279
Pierre Boudes <pierre@mindsized.org>
2023-10-26 17:55:05 +0200

    exo à trous

--- src/main/scala/Exo03.scala
+++ src/main/scala/Exo03.scala
@@ -1,3 +1,10 @@
-class Exo03 {
-  
-}
+package fr.mipn.exo03
+
+sealed trait Messages
+case …
+
+def additions(les prix: ??, msgs : List[Messages]): List[??]
+
+@main def hello: Unit =
+  println("Hello EXO 03!")
+  additions(exemple, exemple).foreach(println)
