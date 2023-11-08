866c597a242be8f376cfd6bc65a7800f57c4f8e9
Marouane <mmouloue@outlook.fr>
2023-11-09 00:47:45 +0100

    Fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,13 @@ package fr.mipn.main
 sealed trait Message
 case class OrderItem(quantity: Int, product: String, table: Int) extends Message
 case class Invoice(table: Int) extends Message
-case object ThankYou extends Message
+case class ThankYou() extends Message
+
 
 def additions(prices: Map[String, Int], messages: List[Message]): List[(Int, Int)] = {
   var totalByTable = Map.empty[Int, Int]
   var result = List.empty[(Int, Int)]
+  var mess = ""
 
   messages.foreach {
     case OrderItem(quantity, product, table) =>
@@ -18,14 +20,14 @@ def additions(prices: Map[String, Int], messages: List[Message]): List[(Int, Int
       val total = totalByTable.getOrElse(table, 0)
       result = result :+ (table, total)
       totalByTable -= table
-    case ThankYou => // Handle thank you message (if needed)
+    case ThankYou() =>
+     mess= "Merci"
   }
 
   result
 }
 @main def hello: Unit =
-  println("Hello world!")
-   val prices = Map("café" -> 3, "thé" -> 12)
+  val prices = Map("café" -> 3, "thé" -> 12)
 
   val exemple = List(
     OrderItem(1, "café", 3),
@@ -34,6 +36,7 @@ def additions(prices: Map[String, Int], messages: List[Message]): List[(Int, Int
     Invoice(3),
     OrderItem(1, "café", 2),
     Invoice(2),
+    ThankYou(),
     OrderItem(2, "café", 3),
     Invoice(3)
   )
